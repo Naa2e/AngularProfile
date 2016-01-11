7749862134ddba02b28db0b858d74daa1a99e548
Nicole Ahima <Nicole.Ahima@outlook.com>
2016-01-11 15:33:41 -0600

    Deleted AngularJS

--- public/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  'use strict';
-
-  angular.module('profileapp', [])
-  .controller('MainController', ['$scope', function($scope){
-    $scope.name="Nicole A";
-  }]);
-})();
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-/*GET*/
-exports.index = function(req, res){
-  res.render('index', { title: "Nicole A", message: "Nicole's Profile Page"});
-};
-
-exports.partials = function(req, res){
-  res.render('partials/' + req.params.name);
-};
